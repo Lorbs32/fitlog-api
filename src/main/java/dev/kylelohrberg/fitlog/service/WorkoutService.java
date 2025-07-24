4fd8b0bffc9057f24adf16cd1ae9319f46f79192
Kyle Lohrberg <lorbs32@gmail.com>
2025-07-23 20:14:50 -0400

    added workout service layer

---
+++ src/main/java/dev/kylelohrberg/fitlog/service/WorkoutService.java
@@ -0,0 +1,33 @@
+package dev.kylelohrberg.fitlog.service;
+
+import dev.kylelohrberg.fitlog.entity.Workout;
+import dev.kylelohrberg.fitlog.repository.WorkoutRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WorkoutService {
+
+    private final WorkoutRepository workoutRepository;
+
+    public WorkoutService(WorkoutRepository workoutRepository){
+        this.workoutRepository = workoutRepository;
+    }
+
+    public Workout createWorkout(Workout workout){
+        return workoutRepository.save(workout);
+    }
+
+    public List<Workout> getAllWorkouts(){
+        return workoutRepository.findAll();
+    }
+
+    public Workout getWorkoutById(Long id){
+        return workoutRepository.findById(id).orElse(null);
+    }
+
+    public void deleteWorkout(Long id){
+        workoutRepository.deleteById(id);
+    }
+}
